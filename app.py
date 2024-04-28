a32d5abf521971aa0f5ba21c6ab90d0f9a521de7
JasonBMa <JasonBMa@users.noreply.github.com>
2024-04-28 01:48:20 -0700

    changes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request, render_template
 import requests 
+from backend import logistic_function
 
 app = Flask(__name__)
 
@@ -10,3 +11,22 @@ def index():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
+
+def parseDataIntoArray(data):
+    return [
+        data['age'],
+        data['bmi'],
+        data['sex'],
+        data['attack'],
+        data['stroke'],
+        data['diabetes'],
+        data['high blood cholesterol levels'],
+        data['cigarettes'],
+        data['alcholic'],
+        data['difficulty'],
+        data['physical'],
+        data['fruit'],
+        data['vegetables'],
+        
+
+    ]
