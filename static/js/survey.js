d0776ac3b1b08caf99ca8a1f926743a827453db9
Saee Patil <saee@Saees-MacBook.local>
2024-04-27 18:39:12 -0700

    slides functionality

---
+++ static/js/survey.js
@@ -0,0 +1,25 @@
+const carousel = document.getElementById('carousel');
+const progressBar = document.querySelector('.progress');
+const slides = document.querySelectorAll('.carousel-slide');
+const totalSlides = slides.length;
+let currentSlide = 0;
+
+function updateCarousel() {
+  const slideWidth = slides[0].offsetWidth;
+  carousel.style.transform = `translateX(${-slideWidth * currentSlide}px)`;
+  progressBar.style.width = `${((currentSlide + 1) / totalSlides) * 100}%`;
+}
+
+
+document.querySelector('.prev-btn').addEventListener('click', () => {
+  currentSlide = Math.max(0, currentSlide - 1);
+  updateCarousel();
+});
+
+document.querySelector('.next-btn').addEventListener('click', () => {
+  currentSlide = Math.min(slides.length - 1, currentSlide + 1);
+  updateCarousel();
+});
+
+updateCarousel(); // Initialize the view
+
